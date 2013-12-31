1db0dc78bd61cba13091b8fee61200a61509521d
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2013-12-31 11:55:52 +0000

    use SHA1 instead of SHA256 on Linux

--- distribute.sh
+++ distribute.sh
@@ -20,7 +20,7 @@ OS=`uname -s`
 if [ "$OS" == "Linux" ]; then
 
     MD5_CMD=md5sum
-    SHA_CMD=sha256sum
+    SHA_CMD=sha1sum
 
 else
     if [ "$OS" == "Darwin" ]; then
@@ -38,10 +38,10 @@ fi
 IN_FILES='server/tsunamid client/tsunami'
 OUT_FILE='hash.txt'
 
-echo "MD5\n" > $OUT_FILE
+echo "MD5" > $OUT_FILE
 $MD5_CMD $IN_FILES >> $OUT_FILE
 
-echo "\nSHA256\n" >> $OUT_FILE
+echo "SHA256" >> $OUT_FILE
 $SHA_CMD $IN_FILES >> $OUT_FILE
 
 #
@@ -50,13 +50,14 @@ $SHA_CMD $IN_FILES >> $OUT_FILE
 #
 
 BUCKET=tsunami-udp
+BUCKET_REGION="eu-west-1"
 TSUNAMI_VERSION=`cat include/tsunami-cvs-buildnr.h  | grep TSUNAMI_CVS_BUILDNR | cut -f 2 -d "\""`
 OS_VERSION=`uname -a | cut -f 3 -d " "`
 
 KEY_PREFIX="$TSUNAMI_VERSION/$OS"_"$OS_VERSION"
-aws s3 cp $OUT_FILE "s3://$BUCKET/$KEY_PREFIX/$OUT_FILE"
+aws s3 cp --region $BUCKET_REGION $OUT_FILE "s3://$BUCKET/$KEY_PREFIX/$OUT_FILE"
 for FILE in $IN_FILES
 do
   FILE_NAME=`echo $FILE | cut -f 2 -d "/"`
-  aws s3 cp $FILE "s3://$BUCKET/$KEY_PREFIX/$FILE_NAME"
+  aws s3 cp --region $BUCKET_REGION $FILE "s3://$BUCKET/$KEY_PREFIX/$FILE_NAME"
 done
