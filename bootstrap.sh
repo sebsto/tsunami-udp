3ad5f5b1525d517648e903a154984dde95b44428
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2014-01-01 13:00:40 +0100

    increase build number before to build

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,14 @@ yum install -y gcc gcc-cpp autoconf automake git
 cd /usr/local
 git clone https://github.com/sebsto/tsunami-udp.git
 
-# compile
+# update build number
 cd tsunami-udp
+TSUNAMI_VERSION=`cat include/tsunami-cvs-buildnr.h  | grep TSUNAMI_CVS_BUILDNR | cut -f 2 -d "\""`
+OLD_BUILD_NUMBER=`echo $TSUNAMI_VERSION | cut -f 4 -d " "`
+let BUILD_NUMBER=OLD_BUILD_NUMBER+1
+sed -e "/TSUNAMI_CVS_BUILDNR/s/$OLD_BUILD_NUMBER/$BUILD_NUMBER/" include/tsunami-cvs-buildnr.h > include/tsunami-cvs-buildnr.h.NEW
+mv include/tsunami-cvs-buildnr.h.NEW include/tsunami-cvs-buildnr.h
+
+# compile
 make
 
